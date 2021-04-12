e7876f22cc5f6fa45a10a095701462bbd6c0dd34
X3ntr _ <cerbersec@gmail.com>
2021-04-13 01:15:46 +0200

    initial commit

---
+++ EarlyBirdAPCInjection/imports.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace EarlyBirdAPCInjection
+{
+    public class imports
+    {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern IntPtr OpenProcess(
+            structs.ProcessAccessFlags dwDesiredAccess,
+            bool bInheritHandle,
+            int dwProcessId);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern IntPtr OpenThread(structs.ThreadAccess dwDesiredAccess, bool bInheritHandle,
+            int dwThreadId);
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr QueueUserAPC(IntPtr pfnAPC, IntPtr hThread, IntPtr dwData);
+
+        [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+        public static extern IntPtr VirtualAllocEx(
+            IntPtr hProcess,
+            IntPtr lpAddress,
+            uint dwSize,
+            structs.AllocationType flAllocationType,
+            structs.MemoryProtection flProtect);
+
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool WriteProcessMemory(
+            IntPtr hProcess,
+            IntPtr lpBaseAddress,
+            byte[] lpBuffer,
+            Int32 nSize,
+            out IntPtr lpNumberOfBytesWritten);
+    }
+}
